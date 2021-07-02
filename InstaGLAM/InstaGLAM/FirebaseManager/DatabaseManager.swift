0a50b5712f70d0fd7faeb1168b6bc349e01c5b54
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-07-02 02:54:43 +0100

    create firebase session manager classes and login screen components

--- InstaGLAM/InstaGLAM/FirebaseManager/DatabaseManager.swift
+++ InstaGLAM/InstaGLAM/FirebaseManager/DatabaseManager.swift
@@ -5,21 +5,13 @@
 //  Created by omokagbo on 02/07/2021.
 //
 
+import Foundation
 import FirebaseDatabase
 
 class DatabaseManager {
+    
     static let shared = DatabaseManager()
-    
-    // MARK:- FUNCTIONS TO MAKE CALLS TO FIREBASE
-    
     private init(){}
     
-    func registerNewUser(fullName: String, username: String, email: String, password: String) {
-        
-    }
-    
-    func loginUser(userName: String?, email: String?, password: String) {
-        
-    }
     
 }
---
+++ InstaGLAM/InstaGLAM/FirebaseManager/StorageManager.swift
@@ -0,0 +1,18 @@
+//
+//  StorageManager.swift
+//  InstaGLAM
+//
+//  Created by omokagbo on 02/07/2021.
+//
+
+import Foundation
+import FirebaseStorage
+
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    private init() {}
+    
+    
+}
