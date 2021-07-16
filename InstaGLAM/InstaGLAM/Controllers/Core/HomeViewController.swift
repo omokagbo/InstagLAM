9243d3610b8e7762efeb8ec8078e0e29e49f6d73
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-07-16 23:44:44 +0100

    set up firebase crashlytics

--- InstaGLAM/InstaGLAM/Controllers/Core/HomeViewController.swift
+++ InstaGLAM/InstaGLAM/Controllers/Core/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  InstaGLAM
 //
 //  Created by omokagbo on 01/07/2021.
@@ -9,9 +9,18 @@ import UIKit
 import FirebaseAuth
 
 final class HomeViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        
+        return table
+    }()
+    
+    // MARK: - View Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupTableView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -19,16 +28,40 @@ final class HomeViewController: UIViewController {
         handleAuthentication()
     }
     
+    // MARK: -  Methods
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
     private func handleAuthentication() {
         // check auth status
         if Auth.auth().currentUser == nil {
             // show login
-//            let loginViewController = LoginViewController()
-//            loginViewController.modalPresentationStyle = .fullScreen
-//            loginViewController.modalTransitionStyle = .crossDissolve
-//            present(loginViewController, animated: true)
             presentLogin()
         }
     }
 }
 
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
